68ec9f5e68d736ca3b4ac792b7ae939e04f35d81
erga-mion <ergamion1997@gmail.com>
2021-04-07 08:57:45 +0900

    second correct answer

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,8 @@
 class FibonacciSequence
   def self.fibonacci_item(n)
+    if n == 0 or n == 1 then
+      return n
+    else
       return fibonacci_item(n - 2) + fibonacci_item(n - 1)
     end
   end
